a9d6227f90199f405ba574a719781ca8cbb2bd79
Balachandar Kuppusamy <dkbalachandar@gmail.com>
2016-02-13 16:08:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+This is a simple program to remove the duplicate numbers in a text file using Cascading Api.
+
+To run this program, follow the below steps,
+
+1. Clone this repository
+
+2. Then build the jar by running mvn package and copy it into hadoop installed directory[/usr/local/hadoop/userLib]. Provide appropriate file permissions
+
+3. Then login as Hadoop user and go to Hadoop directory[/usr/local/hadoop]
+
+4. Create a input text file with some sentences[inputFile.txt] and copy that file to hadoop by running the below command,
+
+      bin/hadoop fs -copyFromLocal inputFile.txt /fc/inputFile.txt
+
+5. Run the jar
+    bin/hadoop jar userLib/cascading-examples3-1.0-jar-with-dependencices.jar /fc/inputFile.txt /fc/output
---
+++ src/main/java/com/cascading/Main.java
@@ -0,0 +1,73 @@
+
+package com.cascading;
+
+import cascading.flow.Flow;
+import cascading.flow.FlowProcess;
+import cascading.flow.hadoop.HadoopFlowConnector;
+import cascading.operation.BaseOperation;
+import cascading.operation.Function;
+import cascading.operation.FunctionCall;
+import cascading.pipe.Each;
+import cascading.pipe.Pipe;
+import cascading.property.AppProps;
+import cascading.scheme.hadoop.TextDelimited;
+import cascading.tap.SinkMode;
+import cascading.tap.Tap;
+import cascading.tap.hadoop.Hfs;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntry;
+
+import java.util.Properties;
+
+/**
+ * A Cascading example to read a text file and convert the string to upper case letters and write into the output file
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hdfs Job is started");
+
+        //input and output path
+        String inputPath = args[0];
+        String outputPath = args[1];
+        System.out.println("inputPath::" + inputPath);
+        System.out.println("outputPath::" + outputPath);
+
+        Properties properties = new Properties();
+        AppProps.setApplicationJarClass(properties, Main.class);
+        //Create the source tap
+        Tap inTap = new Hfs(new TextDelimited(new Fields("line"), true, "\t"), inputPath);
+        //Create the sink tap
+        Tap outTap = new Hfs(new TextDelimited(false, "\t"), outputPath, SinkMode.REPLACE);
+
+        // Pipe to connect Source and Sink Tap
+        Pipe wordsPipe = new Each("words", new UpperCaseFunction(new Fields("line")));
+        HadoopFlowConnector flowConnector = new HadoopFlowConnector(properties);
+        Flow flow = flowConnector.connect("Hdfs Job", inTap, outTap, wordsPipe);
+        flow.complete();
+        System.out.println("Hdfs Job is completed");
+    }
+
+    public static class UpperCaseFunction extends BaseOperation implements Function {
+        private static final long serialVersionUID = 1L;
+
+        public UpperCaseFunction(Fields fields) {
+            super(1, fields);
+        }
+
+        @Override
+        public void operate(FlowProcess flowProcess, FunctionCall functionCall) {
+            TupleEntry arguments = functionCall.getArguments();
+            Tuple tuple = new Tuple();
+            if (arguments == null || arguments.getString(0) == null) {
+                return;
+            }
+            String original = arguments.getString(0).trim();
+            tuple.add(original.toUpperCase());
+            functionCall.getOutputCollector().add(tuple);
+        }
+    }
+}
+
